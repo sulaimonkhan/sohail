29ee1084dedd64d2b806b0657a27e71cebc38040
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-15 17:03:55 +0530

    variable command

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+# if we assign a name to a set of data is called as variable
+# variable=data
+
+# To access variable we use $variable or ${variable}
+
+traine=john
+trainer=steve
+
+echo Devops Trainer - $trainer
+echo aws Trainer - $trainer
