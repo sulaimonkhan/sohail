d2b24e86757ce02f00fa0177909af96ac52264db
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-15 23:20:40 +0530

    function

--- 03-variables.sh
+++ 03-variables.sh
@@ -8,3 +8,16 @@ trainer=steve
 
 echo Devops Trainer - $trainer
 echo aws Trainer - $trainer
+
+
+# variable name can comprise - a-z, A-Z, 0-9,
+# OS Background - All CAPS - VARIABLE_NAME
+# Java Background -camelcase - variableName
+# python Baclground - Snake_case - variable_name 
+# Data -free to give anything , No data type , Everything is string
+# In case if we have special character in data , then do use double quotes 
+# trainer="John Jacob"
+
+# src=x.txt
+# dest=y.txt
+# cp $src $dest
---
+++ 04-function.sh
@@ -0,0 +1,11 @@
+# if we assign a name to a set of commands is called as function
+
+# Declartion function
+
+sample () {
+    echo Hello world from function
+}
+
+# call the function
+
+sample
