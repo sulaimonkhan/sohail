ee81adea91fd50cc392c3e1953f02decf6cfd4c8
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-24 11:17:15 +0530

    loop

--- 07-loop.sh
+++ 07-loop.sh
@@ -7,3 +7,17 @@ for fruit in apple banana orange; do
   #
   #
 done  
+
+
+# while loop
+
+x=10
+while [ $x -gt 0 ]; do
+  echo Value x - $x
+  x=$(($x-1))
+  if [ $x -eq 5 ]; then
+    break
+  fi
+done
+
+# break command is to break and come out of loop. If we use exit it will exit the whole script, But break will just break the loop and next commands after the loop will be executed
